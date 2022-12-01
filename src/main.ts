4fa07e4ac66a62eede02983504316a345ac9e41a
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-11-30 22:55:44 -0300

    Creating validations for installments

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,18 @@ app.post("/transactions", async function (req: Request, res: Response) {
   `, [
     req.body.code, req.body.amount, req.body.numberInstallments, req.body.paymentMethod
   ]);
+  let number = 1;
+  let amount = Math.round((req.body.amount / req.body.numberInstallments) * 100) / 100;
+  let diff = Math.round((req.body.amount - amount * req.body.numberInstallments) * 100) / 100;
 
+  while (number <= req.body.numberInstallments) {
+    if (number === req.body.numberInstallments) {
+      amount += diff;
+    };
+    
+    await connection.query("insert into branas.installments (code, number, amount) values ($1, $2, $3)", [req.body.code, number, amount])
+    number++;
+  };
   await connection.$pool.end();
   res.end();
 });
@@ -21,8 +32,14 @@ app.post("/transactions", async function (req: Request, res: Response) {
 app.get("/transactions/:code", async function(req: Request, res: Response) {
   const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
   const transaction = connection.one("select * from branas.transaction where code = $1", [req.params.code]);
-  transaction.amount = parseFloat(transaction.amount)
-  transaction.paymentMethod = transaction.payment_method = parseFloat(transaction.amount)
+  transaction.amount = parseFloat(transaction.amount);
+  transaction.paymentMethod = transaction.payment_method = parseFloat(transaction.amount);
+  const installments = await connection.query("select * from branas.installment where code = $1", [req.params.code]);
+  for (const installment of installments) {
+    installment.amount = parseFloat(installment.amount);
+  };
+
+  transaction.installments = installments;
   console.log(transaction);
   
   await connection.$pool.end();
